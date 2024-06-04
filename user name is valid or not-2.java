5bf2dd82242cf72cfd93e2a608b68af469f300b0
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-06-04 10:08:52 +0530

    Update user name is valid or not-2.java

--- user name is valid or not-2.java
+++ user name is valid or not-2.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 Scanner input=new Scanner(System.in);
 String s1=input.nextLine();
 String s2=input.nextLine();
-if(s1==s2)
+if(s1==s2){
  System.out.print("user name valid");
+}{
 else
  System.out.print("user name Invalid");
+}
+OUTPUT:
+s1=goutham
+ s2=gotham
+ user name Invalid
