4e2bc4fc7bc0ad34351699594fad83fce6c99422
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-06-04 21:48:28 +0530

    Create 3) ArraySum.java

---
+++ ASSIGNMENT 1/3) ArraySum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+
+        // Input elements into the array
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println("The sum of all elements in the array is: " + sum);
+
+        scanner.close();
+}
+}
+output
+Enter the number of elements in the array: 5
+Enter the elements of the array:
+1
+22
+33
+45
+65
+The sum of all elements in the array is: 166
+
+=== Code Execution Successful ===
