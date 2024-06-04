c95438335894476246acf05d1e5f56bff5781ab0
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-06-04 10:06:37 +0530

    Update vote-4.java

--- vote-4.java
+++ vote-4.java
@@ -1,8 +1,25 @@
-Scanner input=new Scanner(System.in);
-int age=input.nextInt();
-if(age>18)
- System.out.print("you are eligible for vote");
-else if(age<=0)
- System.out.print("Enter the age correctly");
-else
- System.out.print("you are allowed to vote after"+(18-age));
+import java.util.Scanner;
+
+public class Voting {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your age");
+		int age = scan.nextInt();
+		
+		if(age>18){
+			System.out.println("ELIGIBLE to vote");
+		}else{
+			int shortBy = (18 - age);
+			System.out.println("Sorry, you can't vote now! You can vote after :"+ shortBy + " years");
+		}
+
+	}
+
+}
+OUTPUT
+
+Enter your age
+21
+ELIGIBLE to vote
