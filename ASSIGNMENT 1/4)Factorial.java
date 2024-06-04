7eafa99384f2150d56bafa617627f157a16438f0
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-06-04 21:53:59 +0530

    Create 4)Factorial.java

---
+++ ASSIGNMENT 1/4)Factorial.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+
+        long factorial = 1;
+
+        
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of " + number + " is: " + factorial);
+
+        scanner.close();
+}
+}
+output
+Enter a number to calculate its factorial: 10
+The factorial of 10 is: 3628800
+
+=== Code Execution Successful ===
